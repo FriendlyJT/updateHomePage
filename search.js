958647c7f39f12d661580753c3c76baa2097223c
FriendlyJT <48995102+FriendlyJT@users.noreply.github.com>
2022-12-07 19:43:52 +0700

    my web

---
+++ search.js
@@ -0,0 +1,11 @@
+
+
+function search() {
+    var x = document.getElementById("Search");
+  
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
